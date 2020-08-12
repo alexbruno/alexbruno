8df518ed076d4802312c2bd3c9941e914c7b5f20
Alex Bruno Cáceres <git.alexbr@outlook.com>
2020-08-12 12:06:33 -0300

    Content com readme

---
+++ pages/readme.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    <nuxt-content :document="readme" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const readme = await $content('readme').fetch()
+
+    return { readme }
+  },
+}
+</script>
